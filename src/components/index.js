dfb72a199907cb054a3190148a703a43a3078e9a
adharsh <adharsh@maptell.com>
2024-06-20 15:42:04 +0530

    FEATURE: Admin layout navbar and sidebar created

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as Navbar } from './navbar/Navbar';
+export { default as AdminNavbar } from './navbar/AdminNavbar';
+export { default as Sidebar } from './sidebar/Sidebar';
+export { default as AdminSidebar } from './sidebar/AdminSidebar';
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { AdminNavbar, AdminSidebar } from '../components'
 
 const AdminLayout = () => {
   return (
-    <div>AdminLayout</div>
+    <div className='admin'>
+      <div className="admin__container">
+        <div className="admin__navbar">
+          <AdminNavbar/>
+        </div>
+
+        <div className="admin__sidebar">
+          <AdminSidebar/>
+        </div>
+
+        <div className="admin__body">
+          <Outlet />
+        </div>
+      </div>
+    </div>
   )
 }
 
