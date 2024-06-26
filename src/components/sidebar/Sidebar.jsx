1d39ac402aaa20cb28c4586a90c2b7adb9ff8b38
adharsh <adharsh@maptell.com>
2024-06-26 17:55:58 +0530

    FEATURE: Admin dashboard partially saved

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
-import { ArrowCircleRight, ArrowCircleLeft } from '@phosphor-icons/react'
-import React, { useEffect, useState } from 'react'
+import { ArrowCircleRight, ArrowCircleLeft, SignOut, ListMagnifyingGlass } from '@phosphor-icons/react'
+import React from 'react'
+import profileImg from '../../assets/images/profile.png'
+import { MagnifyingGlass } from 'react-loader-spinner'
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
 
@@ -12,10 +14,55 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
 
   return (
     <div className={`sidebar ${!isOpen && 'offSidebar'}`}>
+      {/* middle button to close and open */}
       <div className="sidebar__shutter" onClick={() => handleSidebar()}>
         {
           isOpen ? <ArrowCircleRight size={32} color='grey' /> : <ArrowCircleLeft size={32} color='grey' />
         }
+      </div>
+
+
+      <div className="sidebar__container">
+        <div className="sidebar__logo">
+          <h1>LOGO</h1>
+        </div>
+
+        <div className="sidebar__search">
+          <input type="text" placeholder='Search here..' />
+        </div>
+
+        <div className="sidebar__routes">
+          
+        </div>
+
+        <div className="sidebar__profile">
+          <div className="sidebar__profile--logo">
+            <img src={profileImg} alt="profile-picture" />
+          </div>
+
+          <div className="sidebar__profile--name">
+            <p><span>hello, </span>Rajan David</p>
+            <p>helloadmin@gmail.com</p>
+          </div>
+
+          <div className="sidebar__profile--logout">
+            <button>
+              <SignOut size={28} color="white" weight="fill" />
+            </button>
+          </div>
+        </div>
+
+        <div className="sidebar__support">
+          <div className="sidebar__support--text">
+            <h4>Help and Support</h4>
+            <p>support@affiliatemarketting.com</p>
+          </div>
+
+          <div className="sidebar__support--social">
+            Facebook
+          </div>
+
+        </div>
       </div>
     </div>
   )
