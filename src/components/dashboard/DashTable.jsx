9d103b8c8f2d1ceb0cddaa53215b83034ebc6ef4
adharsh <adharsh@maptell.com>
2024-07-02 11:27:14 +0530

    FEATURE: Dashboard Table

--- src/components/dashboard/DashTable.jsx
+++ src/components/dashboard/DashTable.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Button, Table } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
-const DashTable = ({}) => {
+const DashTable = ({ heading, columns, data, path }) => {
+  const navigate = useNavigate()
+
   return (
-    <div className='dashtable'>DashTable</div>
+    <div className='dashtable'>
+      <div className="dashtable__heading">
+        <p>{heading}</p>
+        <Button
+          size='small'
+          onClick={() => path && navigate(path)}
+        >
+          View All
+        </Button>
+      </div>
+      <div className="dashtable__table">
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+        />
+      </div>
+    </div >
   )
 }
 
