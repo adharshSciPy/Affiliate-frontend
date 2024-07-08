e8ba23790ca05cc1d81cdc11373dd7a84d9d0d75
adharsh <adharsh@maptell.com>
2024-07-08 13:53:43 +0530

    FEATURE: Active companies and new compnaies styles created in admin

--- src/views/admin/CompanyVerifications.jsx
+++ src/views/admin/CompanyVerifications.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import { Avatar, Button, Table } from 'antd'
+import { UserOutlined } from '@ant-design/icons';
 
 const CompanyVerifications = () => {
+
+  const columns = [
+    {
+      title: 'Avatar',
+      dataIndex: 'avatar',
+      key: 'avatar',
+    },
+    {
+      title: 'Full Name',
+      dataIndex: 'fullName',
+      key: 'fullName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '',
+      dataIndex: 'button',
+      key: 'button',
+    },
+  ];
+
+  const tableData = [
+    {
+      avatar: <Avatar size="large" icon={<UserOutlined />} />,
+      fullName: 'Brown',
+      email: 'brown@gmail.com',
+      address: 'New York No. 1 Lake Park',
+      button: <Button type='primary'>Verify</Button>
+    },
+    {
+      avatar: <Avatar size="large" icon={<UserOutlined />} />,
+      fullName: 'Green',
+      email: 'green@gmail.com',
+      address: 'London No. 1 Lake Park',
+      button: <Button type='primary'>Verify</Button>
+    },
+    {
+      avatar: <Avatar size="large" icon={<UserOutlined />} />,
+      fullName: 'Black',
+      email: 'black@green.com',
+      button: <Button type='primary'>Verify</Button>
+    },
+  ];
+
   return (
-    <div>CompanyVerifications</div>
+    <div className='newcompanies'>
+      <div className="newcompanies__header">
+        <h3>New Requests</h3>
+      </div>
+
+      <div className="newcompanies__table">
+        <Table
+          columns={columns}
+          dataSource={tableData}
+        />
+      </div>
+    </div>
   )
 }
 
