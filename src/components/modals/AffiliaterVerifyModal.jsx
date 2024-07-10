0cae5e8e87be6807dbe07e57ff550bbc08271309
vishvascipy <vishva.scipy@gmail.com>
2024-07-10 16:13:18 +0530

    FEATURE: verify modal

--- src/components/modals/AffiliaterVerifyModal.jsx
+++ src/components/modals/AffiliaterVerifyModal.jsx
@@ -8,16 +8,25 @@ const AffiliaterVerifyModal = ({ isModal, modalData, setIsModal, verify }) => {
     }
 
     return (
-        <Modal
-            centered
-            open={isModal}
-            width={400}
-            onOk={() => verify()}
-            onCancel={() => close()}
-            footer={null}
-        >
-            <Button type='primary' onClick={() => verify()}>Verify</Button>
-        </Modal>
+        <div className="verifymodal">
+            <Modal
+                centered
+                open={isModal}
+                width={400}
+                onOk={() => verify()}
+                onCancel={() => close()}
+                footer={null}
+            >
+                <div className="verifymodal__text">
+                    <b>Are you sure you want to verify this ?</b>
+                    <p>Verifying will confirm their identity and grant them full access to all features.</p>
+                </div>
+                <div className="verifymodal__container">
+                    <Button onClick={() => close()}>Cancel</Button>
+                    <Button type='primary' onClick={() => verify()}>Verify</Button>
+                </div>
+            </Modal>
+        </div>
     )
 }
 
