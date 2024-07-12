17295fa42e85daa1d3fa253cb2d761234777e4d3
adharsh <adharsh@maptell.com>
2024-07-12 14:43:37 +0530

    FEATURE: Affiliater page basic style created

--- src/views/affiliater/AffiliaterRegistrationPage.jsx
+++ src/views/affiliater/AffiliaterRegistrationPage.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Navbar } from '../../components'
+import bgCoverImg from '../../assets/images/affreg-coverimage.jpg'
+import { Button } from 'antd'
 
 const AffiliaterRegistrationPage = () => {
   return (
     <>
-      <Navbar />
-      <div>AffiliaterRegistrationPage</div>
+      <div className='affiliaterregpage'
+        style={{
+          background: `url(${bgCoverImg})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+          <div className='affiliaterregpage__container'>
+            <div className="affiliaterregpage__header">
+              <div className="affiliaterregpage__header--text">
+                <h3>Tell us more about yourself</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, error?</p>
+              </div>
+
+              <div className="affiliaterregpage__header--logout">
+                <Button>Logout</Button>
+              </div>
+            </div>
+          </div>
+      </div>
     </>
   )
 }
