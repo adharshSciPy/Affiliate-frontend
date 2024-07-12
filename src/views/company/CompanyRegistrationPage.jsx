861b351d9a14bfc23bc9f38e50aeb87f1e61180e
adharsh <adharsh@maptell.com>
2024-07-12 13:19:57 +0530

    FEATURE: Company registration page created and styled

--- src/views/company/CompanyRegistrationPage.jsx
+++ src/views/company/CompanyRegistrationPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Navbar } from '../../components'
+import bgCoverImg from '../../assets/images/reg-coverimage.jpg'
 
 const CompanyRegistrationPage = () => {
   return (
     <>
-      <Navbar />
-      <div>CompanyRegistrationPage</div>
+      <div className='companyregpage'
+        style={{
+          background: `url(${bgCoverImg})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className='companyregpage__container'>
+
+          <div className="companyregpage__header">
+            <h3>Tell us more about your company</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, error?</p>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
