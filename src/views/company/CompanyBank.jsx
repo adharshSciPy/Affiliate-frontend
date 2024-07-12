ba25c74c8325abd3a408d633ef798a955a35669e
vishvascipy <vishva.scipy@gmail.com>
2024-07-12 16:48:21 +0530

    FEATURE: personal information

---
+++ src/views/company/CompanyBank.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CompanyBank() {
+  return (
+    <div>CompanyBank</div>
+  )
+}
+
+export default CompanyBank
---
+++ src/views/company/CompanyProof.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CompanyProof() {
+  return (
+    <div>CompanyProof</div>
+  )
+}
+
+export default CompanyProof
