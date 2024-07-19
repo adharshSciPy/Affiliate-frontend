b3455ee132f397913e576f7fd7457b2d9a8b23f6
nandana-2002 <nnandana429@gmail.com>
2024-07-19 14:08:37 +0530

    FEATURE:

---
+++ src/views/admin/AdminProof.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from "antd";
+
+function AdminProof() {
+  return (
+    <div className='adminproof'>
+        <div className="adminproof__childs">
+        <p> Utility Bills : <Button>Upload</Button></p>
+
+        <p>Bank Statement : <Button>Upload</Button> </p>
+        
+        </div>
+       
+      
+    </div>
+  )
+}
+
+export default AdminProof
