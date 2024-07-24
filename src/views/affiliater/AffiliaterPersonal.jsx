20d90ab15687d37a7bf50cf55ec41ceb1e37ed1d
arya <aryavs819@gmail.com>
2024-07-24 13:52:04 +0530

    FEATURE:modified in affilate reg page

--- src/views/affiliater/AffiliaterPersonal.jsx
+++ src/views/affiliater/AffiliaterPersonal.jsx
@@ -66,143 +66,145 @@ function AffiliaterPersonal() {
   return (
     <>
       <div className="affiliaterpersonal">
-        <div className="affiliaterpersonal__containerone">
-          <div className="affiliaterpersonal__containerone--input">
-            <div className="affiliaterpersonal__containerone--label">
-              <p>First Name</p>
+        <div className="affiliaterpersonal__containers">
+          <div className="affiliaterpersonal__containerone">
+            <div className="affiliaterpersonal__containerone--input">
+              <div className="affiliaterpersonal__containerone--label">
+                <p>First Name</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="John"
+                  name="firstName"
+                  value={form.firstName}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                placeholder="John"
-                name="firstName"
-                value={form.firstName}
-                onChange={handleChange}
-                size="medium"
-              />
-            </div>
-          </div>
 
-          <div className="companypersonal__containerone--input">
-            <div className="companypersonal__containerone--label">
-              <p>Date of Birth</p>
+            <div className="affiliaterpersonal__containerone--input">
+              <div className="affiliaterpersonal__containerone--label">
+                <p>Date of Birth</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="dd/mm/yyyy"
+                  name="DOB"
+                  value={form.DOB}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                placeholder="dd/mm/yyyy"
-                name="DOB"
-                value={form.DOB}
-                onChange={handleChange}
-                size="medium"
-              />
-            </div>
-          </div>
 
-          <div className="affiliaterpersonal__containerone--input">
-            <div className="affiliaterpersonal__containerone--label">
-              <p>Gender</p>
+            <div className="affiliaterpersonal__containerone--input">
+              <div className="affiliaterpersonal__containerone--label">
+                <p>Gender</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="Male/Female"
+                  name="Gender"
+                  value={form.Gender}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                placeholder="Male/Female"
-                name="Gender"
-                value={form.Gender}
-                onChange={handleChange}
-                size="medium"
-              />
+            <div className="affiliaterpersonal__containerone--input">
+              <div className="affiliaterpersonal__containerone--label">
+                <p>Nationality</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="Indian"
+                  name="nationality"
+                  value={form.nationality}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-          </div>
-          <div className="affiliaterpersonal__containerone--input">
-            <div className="affiliaterpersonal__containerone--label">
-              <p>Nationality</p>
-            </div>
-            <div>
-              <Input
-                placeholder="Indian"
-                name="nationality"
-                value={form.nationality}
-                onChange={handleChange}
-                size="medium"
-              />
-            </div>
-          </div>
 
-          <div className="affiliaterpersonal__containerone--input">
-            <div className="affiliaterpersonal__containerone--label">
-              <p>Address</p>
-            </div>
-            <div>
-              <TextArea
-                placeholder="John"
-                name="Address"
-                value={form.Address}
-                onChange={handleChange}
-                size="medium"
-                rows={4}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="affiliaterpersonal__containertwo">
-          <div className="companypersonal__containertwo--input">
-            <div className="companypersonal__containertwo--label">
-              <p>Last Name</p>
-            </div>
-            <div>
-              <Input
-                placeholder="Doe"
-                name="lastName"
-                value={form.lastName}
-                onChange={handleChange}
-                size="medium"
-              />
+            <div className="affiliaterpersonal__containerone--input">
+              <div className="affiliaterpersonal__containerone--label">
+                <p>Address</p>
+              </div>
+              <div>
+                <TextArea
+                  placeholder="John"
+                  name="Address"
+                  value={form.Address}
+                  onChange={handleChange}
+                  size="medium"
+                  rows={4}
+                />
+              </div>
             </div>
           </div>
-          <div className="affiliaterpersonal__containertwo--input">
-            <div className="affiliaterpersonal__containertwo--label">
-              <p>Phone Number</p>
+          <div className="affiliaterpersonal__containertwo">
+            <div className="affiliaterpersonal__containertwo--input">
+              <div className="affiliaterpersonal__containertwo--label">
+                <p>Last Name</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="Doe"
+                  name="lastName"
+                  value={form.lastName}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                placeholder="9874445631"
-                name="phoneNumber"
-                value={form.phoneNumber}
-                onChange={handleChange}
-                size="medium"
-              />
+            <div className="affiliaterpersonal__containertwo--input">
+              <div className="affiliaterpersonal__containertwo--label">
+                <p>Phone Number</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="9874445631"
+                  name="phoneNumber"
+                  value={form.phoneNumber}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-          </div>
-          <div className="affiliaterpersonal__containertwo--input">
-            <div className="companypersonal__containertwo--label">
-              <p>Email address</p>
+            <div className="affiliaterpersonal__containertwo--input">
+              <div className="affiliaterpersonal__containertwo--label">
+                <p>Email address</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="example@gmail.com"
+                  name="emailAddress"
+                  value={form.emailAddress}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                placeholder="example@gmail.com"
-                name="emailAddress"
-                value={form.emailAddress}
-                onChange={handleChange}
-                size="medium"
-              />
+            <div className="affiliaterpersonal__containertwo--input">
+              <div className="affiliaterpersonal__containertwo--label">
+                <p>Website</p>
+              </div>
+              <div>
+                <Input
+                  placeholder="example@gmail.com"
+                  name="website"
+                  value={form.website}
+                  onChange={handleChange}
+                  size="medium"
+                />
+              </div>
             </div>
-          </div>
-          <div className="affiliaterpersonal__containertwo--input">
-            <div className="affiliaterpersonal__containertwo--label">
-              <p>Website</p>
+            <div className="affiliaterpersonal__containertwo--button">
+              <Button type="primary" success onClick={handleSubmit}>
+                Next
+              </Button>
             </div>
-            <div>
-              <Input
-                placeholder="example@gmail.com"
-                name="website"
-                value={form.website}
-                onChange={handleChange}
-                size="medium"
-              />
-            </div>
-          </div>
-          <div className="affiliaterpersonal__containertwo--button">
-            <Button type="primary" success onClick={handleSubmit}>
-              Next
-            </Button>
           </div>
         </div>
       </div>
