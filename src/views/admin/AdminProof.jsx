3fcc7eccbf34a7eb0b4fc08bf1a530a09737a55d
adharsh <adharsh@maptell.com>
2024-08-05 17:04:26 +0530

    FEATURE: upload completed

--- src/views/admin/AdminProof.jsx
+++ src/views/admin/AdminProof.jsx
@@ -1,47 +1,73 @@
-import React from 'react'
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, message, Upload } from 'antd';
+import React, { useState } from 'react';
+import { Button, message, Input } from 'antd';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useNotification } from '../../context/NotificationContext'
 
 function AdminProof() {
+  const params = useParams();
+  const { notification } = useNotification();
+  const affiliaterId = params?.id;
+  const [files, setFiles] = useState({
+    uploads: null,
+    bankStatement: null,
+    idDocument: null
+  });
 
-  const props = {
-    name: 'file',
-    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    headers: {
-      authorization: 'authorization-text',
-    },
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
+  const handleChange = (field) => (event) => {
+    setFiles(prevFiles => ({
+      ...prevFiles,
+      [field]: event.target.files[0]
+    }));
+
+  };
+
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    Object.keys(files).forEach(key => {
+      if (files[key]) {
+        formData.append('uploads[]', files[key]);
       }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
+    });
+
+    try {
+      const result = await axios.patch(`http://localhost:8000/api/v1/user/affiliaters/${affiliaterId}/documents`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      notification("success",
+        "Files Updation Succesfull",
+        result?.data?.message,
+        "bottomRight");
+
+    } catch (error) {
+      notification(
+        "error",
+        "Files Updation Failed",
+        error?.data?.message,
+        "bottomRight"
+      );
+    }
   };
 
   return (
     <div className='adminproof'>
       <div className="adminproof__childs">
-        <p> Utility Bills :   <Upload {...props}>
-          <Button style={{ marginLeft: '5px' }} icon={<UploadOutlined />}>Upload</Button>
-        </Upload></p>
-
-        <p>Bank Statement :   <Upload {...props}>
-          <Button style={{ marginLeft: '5px' }} icon={<UploadOutlined />}>Upload</Button>
-        </Upload></p>
-
-        <p>Identification Document :   <Upload {...props}>
-          <Button style={{ marginLeft: '5px' }} icon={<UploadOutlined />}>Upload</Button>
-        </Upload></p>
-
+        <p>Utility Bills:
+          <Input type='file' name='uploads' onChange={handleChange('utilityBill')} />
+        </p>
+        <p>Bank Statement:
+          <Input type='file' name='uploads' onChange={handleChange('bankStatement')} />
+        </p>
+        <p>Identification Document:
+          <Input type='file' name='uploads' onChange={handleChange('idDocument')} />
+        </p>
+        <Button style={{ marginTop: '20px' }} onClick={handleSubmit}>Upload</Button>
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default AdminProof
+export default AdminProof;
