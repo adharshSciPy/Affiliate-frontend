f85ff2eaebdb15b367738fe20066fee490e7f758
arya <aryavs819@gmail.com>
2024-08-12 16:12:25 +0530

    FEATURE:affiliater help created,affiliater css and affiliater notification modified

--- src/views/affiliater/AffiliaterNotification.jsx
+++ src/views/affiliater/AffiliaterNotification.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Button } from "antd";
 
 function AffiliaterNotification() {
     return (
         <div className='affiliaternotification'>
-            <div className="affiliaternotification__container">Affiliater Notification</div>
+            <div className="affiliaternotification__buttoncontainer">
+                <div className="affiliaterhelp__buttongreen">
+                <Button type='primary' success>On</Button>
+                </div>
+                <div className="affiliaterhelp__buttongrey">
+                <Button className='off' style={{backgroundColor:'var(--text-grey)'}} type='primary' >Off</Button>
+                </div>
+            </div>
+            <div className="affiliaternotification__notification">
+                <h2>Notifications</h2>
+            </div>
         </div>
     )
 }
