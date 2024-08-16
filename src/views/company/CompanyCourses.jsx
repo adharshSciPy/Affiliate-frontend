5f6cd8bd87e6c8d5bf809c193cb7f0e40bd69cab
adharsh <adharsh@maptell.com>
2024-08-16 17:07:59 +0530

    MODIFICATION: course modifing

--- src/views/company/CompanyCourses.jsx
+++ src/views/company/CompanyCourses.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Select, Input, Descriptions } from 'antd';
+import { Select, Input, Button } from 'antd';
 
 const { Option } = Select;
 
@@ -51,65 +51,159 @@ function CompanyCourses() {
     setForm({ ...form, [name]: value });
   }
 
+  //handlesubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = {
+        courseCategory: form.courseCategory,
+        courseName: form.courseName,
+        courseDescription:form.courseDescription,
+        courseDuration:form.courseDuration,
+        certificateOffered:form.certificateOffered,
+        courseFee:form.courseFee,
+        offerFee:form.offerFee,
+        addHeading:form.addHeading,
+        description:form.description,
+      };
+      let companyId = logId;
+      console.log("companyId", companyId);
+      console.log("payload", payload);
+      const result = await identificationdetails({ companyId, payload });
+      console.log("result", result);
+      if (result) {
+        notification(
+          "success",
+          "Registration Succesfull",
+          result?.data?.message,
+          "bottomRight"
+        );
+        setForm(fields);
+      }
+    } catch (error) {
+      notification(
+        "error",
+        "Registration Failed",
+        error?.data?.message,
+        "bottomRight"
+      );
+    }
+  };
+
   return (
     <div className='course'>
       <div className='course__container'>
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Course Category: {' '}
 
-        <p>Course Category:
-          <Select
-            style={{ width: 200 }}
-            placeholder="Select a Category"
-            onChange={handleCategoryChange}
-            value={selectedCategory || undefined}
-          >
-            <Option value="Medicine">Medicine</Option>
-            <Option value="Engineering">Engineering</Option>
-            <Option value="IT">IT</Option>
-            <Option value="Commerce">Commerce</Option>
-            <Option value="Other">Other</Option>
-          </Select>
+              <Select
+                style={{ width: 200 }}
+                placeholder="Select a Category"
+                onChange={handleCategoryChange}
+                value={selectedCategory || undefined}
+              >
+                <Option value="Medicine">Medicine</Option>
+                <Option value="Engineering">Engineering</Option>
+                <Option value="IT">IT</Option>
+                <Option value="Commerce">Commerce</Option>
+                <Option value="Other">Other</Option>
+              </Select>
 
-          {isOtherSelected && (
-            <Input
-              type='text'
-              name='courseCategory'
-              style={{ marginTop: '10px', width: 200 }}
-              placeholder="Please specify"
-              value={otherCategory}
-              onChange={handleOtherCategoryChange}
-            />
-          )}
-        </p>
-        <p>Image: {' '}
-          <Input style={{ width: '300px' }} type='file' name='uploads' onChange={handleChange('image')} />
-        </p>
-        <p>Course Name:
-          <Input type='text' name='courseName' onChange={formChange} />
-        </p>
-        <p>Course Description: {' '}
-          <Input type='text' name='courseDescription' onChange={formChange} />
-        </p>
-        <p>Course Duration: {' '}
-          <Input type='text' name='courseDuration' onChange={formChange} />
-        </p>
-        <p>Certificate Offered: {' '}
-          <Input type='text' name='certificateOffered' onChange={formChange} />
-        </p>
-        <p>Course Fee:{' '}
-          <Input type='text' name='courseFee' onChange={formChange} />
-        </p>
-        <p>Offer Fee: {' '}
-          <Input type='text' name='offerFee' onChange={formChange} />
-        </p>
-        <p style={{ fontSize: '20px', fontWeight: '700', color:'var(--primary-color)'}}>Syllabus</p>
-        <p>Add Heading: {' '}
-          <Input type='text' name='addHeading' onChange={formChange} />
-        </p>
-        <p>Description: {' '}
-          <Input type='text' name='description' onChange={formChange} />
-        </p>
+              {isOtherSelected && (
+                <Input
+                  type='text'
+                  name='courseCategory'
+                  style={{ marginTop: '10px', width: 200 }}
+                  placeholder="Please specify"
+                  value={otherCategory}
+                  onChange={handleOtherCategoryChange}
+                />
+              )}
+            </p>
+          </div>
+        </div>
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Image: {' '}
+              <Input style={{ width: '300px' }} type='file' name='uploads' onChange={handleChange('image')} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Course Name:
+              <Input type='text' name='courseName' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Course Description:
+              <Input type='text' name='courseDescription' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Course Duration:
+              <Input type='text' name='courseDuration' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Certificate Offered:
+              <Input type='text' name='certificateOffered' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Course Fee:
+              <Input type='text' name='certificateOffered' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Offer Fee:
+              <Input type='text' name='offerFee' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p style={{ fontSize: '20px', fontWeight: '700', color: 'var(--primary-color)' }}>Syllabus</p>
+            <p>Add Heading:
+              <Input type='text' name='addHeading' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="course__container--input">
+          <div className="course__container--label">
+            <p>Description:
+              <Input type='text' name='description' onChange={formChange} />
+            </p>
+          </div>
+        </div>
+
+        <div className="studyabroad__container--button">
+          <Button type="primary" success onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+
       </div>
-    </div>
+    </div >
   );
 }
 
